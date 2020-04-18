be917dd73641766e3ff13d8aac3dc9cbfcaaef78
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-18 16:43:34 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,16 +1,11 @@
 class Book 
   
-  def initialize(title, author, count, genre)
+  def initialize(title)
     @title = title
-    @author = author
-    @count = count 
-    @genre = genre
   end
   
   attr_accessor :title 
-  attr_accessor :author
-  attr_accessor :count
-  attr_accessor :genre
+
   
   
 end
